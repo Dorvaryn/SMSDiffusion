a1f9e78f9292653485820d21ecbbf29d0272832d
Neamar <neamar@neamar.fr>
2013-02-27 18:45:15 +0100

    Use Fragment instead of Activity

--- src/fr/odai/smsdiffusion/DiffusionContactFragment.java
+++ src/fr/odai/smsdiffusion/DiffusionContactFragment.java
@@ -1,39 +1,29 @@
 package fr.odai.smsdiffusion;
 
-import android.database.Cursor;
+import java.util.ArrayList;
+
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.provider.ContactsContract.CommonDataKinds;
 import android.support.v4.app.ListFragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import fr.odai.smsdiffusion.adapter.ContactAdapter;
+import fr.odai.smsdiffusion.adapter.POJOContact;
 
 public class DiffusionContactFragment extends ListFragment {
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View root = inflater.inflate(R.layout.activity_diffusion_contact, container, false);
+
 		AutoCompleteTextView phoneNumber = (AutoCompleteTextView) root.findViewById(R.id.text_contact);
 
-		ArrayAdapter<String> acontactslist = new ArrayAdapter<String>(getActivity(), R.layout.list_item_contacts);
-		Cursor contactCursor = getActivity().getContentResolver().query(
-				ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
-		contactCursor.moveToFirst();
-		do {
-			String name = contactCursor.getString(contactCursor
-					.getColumnIndex(CommonDataKinds.Phone.DISPLAY_NAME));
-			String num = contactCursor.getString(contactCursor
-					.getColumnIndex(CommonDataKinds.Phone.NUMBER));
-			acontactslist.add(num);
-			Log.e("wtf", num);
-		} while (contactCursor.moveToNext());
+		ArrayList<POJOContact> allContacts = POJOContact.getAllContacts(getActivity());
+		ContactAdapter adapter = new ContactAdapter(getActivity(), R.layout.item_contact,
+				allContacts);
 
-		phoneNumber.setAdapter(acontactslist);
+		phoneNumber.setAdapter(adapter);
 		return root;
 	}
 }
