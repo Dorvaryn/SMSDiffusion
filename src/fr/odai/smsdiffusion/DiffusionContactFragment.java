cc503b65ba7b3854898417712cf266adc2350ad1
Neamar <neamar@neamar.fr>
2013-02-27 20:14:18 +0100

    Swipe to delete (draft)

--- src/fr/odai/smsdiffusion/DiffusionContactFragment.java
+++ src/fr/odai/smsdiffusion/DiffusionContactFragment.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
-import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -16,34 +17,64 @@ import fr.odai.smsdiffusion.adapter.POJOContact;
 
 public class DiffusionContactFragment extends ListFragment {
 
+	public OnTouchListener gestureListener;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View root = inflater.inflate(R.layout.activity_diffusion_contact, container, false);
 
+		gestureListener = new View.OnTouchListener() {
+			private int padding = 0;
+			private int initialx = 0;
+			private int currentx = 0;
+
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					padding = 0;
+					initialx = (int) event.getX();
+					currentx = (int) event.getX();
+				}
+				if (event.getAction() == MotionEvent.ACTION_MOVE) {
+					currentx = (int) event.getX();
+					padding = currentx - initialx;
+				}
+
+				if (event.getAction() == MotionEvent.ACTION_UP
+						|| event.getAction() == MotionEvent.ACTION_CANCEL) {
+					padding = 0;
+					initialx = 0;
+					currentx = 0;
+				}
+
+				v.setPadding(padding, 0, 0, 0);
+
+				return true;
+			}
+		};
 
 		// List adapter
-		final ContactAdapter adapter = new ContactAdapter(getActivity(), R.layout.item_contact, new ArrayList<POJOContact>());
+		final ContactAdapter adapter = new ContactAdapter(getActivity(), R.layout.item_contact,
+				new ArrayList<POJOContact>(), gestureListener);
 		setListAdapter(adapter);
-		
+
 		// Autocomplete adapter
-		final AutoCompleteTextView phoneNumber = (AutoCompleteTextView) root.findViewById(R.id.text_contact);
+		final AutoCompleteTextView phoneNumber = (AutoCompleteTextView) root
+				.findViewById(R.id.text_contact);
 		ArrayList<POJOContact> allContacts = POJOContact.getAllContacts(getActivity());
 		final ContactAdapter autoAdapter = new ContactAdapter(getActivity(), R.layout.item_contact,
-				allContacts);
-		
+				allContacts, null);
 
 		phoneNumber.setAdapter(autoAdapter);
 		phoneNumber.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> contactAdapter, View arg1, int position, long arg3) {
+			public void onItemClick(AdapterView<?> contactAdapter, View arg1, int position,
+					long arg3) {
 				adapter.add(autoAdapter.getItem(position));
 				phoneNumber.setText("");
 			}
 		});
-		
 
-		
 		return root;
 	}
 }
--- src/fr/odai/smsdiffusion/adapter/ContactAdapter.java
+++ src/fr/odai/smsdiffusion/adapter/ContactAdapter.java
@@ -2,25 +2,28 @@ package fr.odai.smsdiffusion.adapter;
 
 import java.util.ArrayList;
 
-import fr.odai.smsdiffusion.R;
-
 import android.content.Context;
 import android.net.Uri;
 import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.QuickContactBadge;
 import android.widget.TextView;
+import fr.odai.smsdiffusion.R;
 
 public class ContactAdapter extends ArrayAdapter<POJOContact> {
 
 	public ArrayList<POJOContact> contacts;
+	private OnTouchListener listener;
 
-	public ContactAdapter(Context context, int textViewResourceId, ArrayList<POJOContact> contacts) {
+	public ContactAdapter(Context context, int textViewResourceId, ArrayList<POJOContact> contacts,
+			OnTouchListener listener) {
 		super(context, textViewResourceId, contacts);
 		this.contacts = contacts;
+		this.listener = listener;
 	}
 
 	@Override
@@ -37,19 +40,22 @@ public class ContactAdapter extends ArrayAdapter<POJOContact> {
 
 		TextView phone = (TextView) convertView.findViewById(R.id.item_contact_phone);
 		phone.setText(contact.phone);
-		
+
 		TextView phoneType = (TextView) convertView.findViewById(R.id.item_contact_phone_type);
 		phoneType.setText(contact.phoneType);
 
-		QuickContactBadge badge = (QuickContactBadge) convertView.findViewById(R.id.item_contact_badge);
+		QuickContactBadge badge = (QuickContactBadge) convertView
+				.findViewById(R.id.item_contact_badge);
 		if (contact.icon != null)
 			badge.setImageURI(contact.icon);
 		else
 			badge.setImageDrawable(getContext().getResources().getDrawable(R.drawable.ic_contact));
-		badge.assignContactUri(Uri.withAppendedPath(
-				ContactsContract.Contacts.CONTENT_LOOKUP_URI,
+		badge.assignContactUri(Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_LOOKUP_URI,
 				String.valueOf(contact.lookupKey)));
-		
+
+		if (this.listener != null)
+			convertView.setOnTouchListener(this.listener);
+
 		return convertView;
 	}
 
